87438028334cb8046117ec223aabc9c48538c841
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-03-27 23:56:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+// const hike = 43
+// function distanceFromHqInBlocks(hike){
+//     let walk = 42
+//     if (walk > hike);
+//     return 8
+    //return walk = 8 
+    //return distanceFromHqInBlocks (43)
+const pickupLocation = (40);
+function distanceFromHqInBlocks(pickupLocation){
+    const blocksTraveled = (42 - pickupLocation);
+    return blocksTraveled;
+}
+
+let distanceFromHqInFeet = blocksTraveled * 264
+
+function distanceFromHqInFeet(distanceInFeet){
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(){}
